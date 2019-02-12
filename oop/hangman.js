a1930381359b3743c0954ea414e6d7d229cf7b33
Isaac Fonseca <isaac@launchxd.com>
2019-02-12 12:55:04 -0500

    added hangman game file

--- oop/hangman.js
+++ oop/hangman.js
@@ -4,14 +4,26 @@
 
 
 const Game = function (word, remainingGuesses) {
-    this.name = word
+    this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
+    this.guessedLetters = ['c']
 }
 
-
+Game.prototype.getPuzzle = function () {
+    let puzzle = ''
+    
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+        } else {
+            puzzle += '*'
+        }
+    })
+    return puzzle
+}
 
 const game1 = new Game('Cat', 2)
-console.log(game1)
+console.log(game1.getPuzzle())
 
 const game2 = new Game('New York', 4)
-console.log(game2)
+console.log(game2.getPuzzle())
